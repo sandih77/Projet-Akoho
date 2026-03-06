84005a04bc2ec950f041a1d974201a01f79e8d58
RANDRIANARIJAONA Sandih Finaritra <sandihrandrianarijaona@gmail.com>
2026-03-06 11:28:37 +0300

    formulaire de Race

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RaceForm } from './components/race-form/race-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'race-form', component: RaceForm }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { RaceForm } from './components/race-form/race-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
